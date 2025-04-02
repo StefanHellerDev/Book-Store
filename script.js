ee2153846ac61c859c1022e728254ca78d939068
Stefan Heller <S_Heller@gmx.de>
2025-04-02 21:26:29 +0200

    Working on the body

--- script.js
+++ script.js
@@ -5,10 +5,23 @@ function init() {
   }
 }
 
-console.table(books);
+function filledHeart() {
+  document
+    .getElementById("heart")
+    .setAttribute("src", "./assets/icons/heart_filled.png");
+  return;
+}
+
+function unfilledHeart() {
+  document
+    .getElementById("heart")
+    .setAttribute("src", "./assets/icons/heart_unfilled.png");
+  return;
+}
 
 function bookTemplate(bookstore, i) {
   bookstore.innerHTML += `<div>
     <img class="grow" src="./img/${imagesSmall[i]}.jpg" onclick="switchOnOverlay(${i})">
     </div>`;
 }
+
