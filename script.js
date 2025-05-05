8b9197ba1e220bfe43ae92a8147610cdd5214c3b
Stefan Heller <S_Heller@gmx.de>
2025-05-05 21:42:52 +0200

    Minor works

--- script.js
+++ script.js
@@ -23,57 +23,6 @@ function unfilledHeart(bookIndex) {
   return;
 }
 
-function bookTemplate(bookstore, bookIndex, bookPrice) {
-  bookstore.innerHTML += `
-        <div class="bookCard">
-          <div class="bookCardTitle">${books[bookIndex].name}</div>
-          <div class="bookCardPic">
-            <img class="bookCardIcon" src="./assets/icons/cardBook.png" alt="Placeholder book">
-          </div>
-          <div class="priceLikesContainer">
-            <div class="bookPrice">${bookPrice}</div>
-            <div class="d_flex ai_center">
-              <p id="bookLikes${bookIndex}">${books[bookIndex].likes}</p>
-              <img id="heart${bookIndex}" src="" onclick="toggleHeart(${bookIndex})">
-            </div>
-          </div>
-          <div class="authorYearGenreContainer">
-            <table>
-              <tr>
-                <td>Autor </td>
-                <td> : </td>
-                <td> ${books[bookIndex].author}</td>
-              </tr>
-              <tr>
-                <td>Erscheinungsjahr </td>
-                <td> : </td>
-                <td> ${books[bookIndex].publishedYear}</td>
-              </tr>
-              <tr>
-                <td>Genre </td>
-                <td> : </td>
-                <td> ${books[bookIndex].genre}</td>
-              </tr>
-            </table>
-          </div>
-          <div class="commentSection">
-            <p>Kommentare:</p>
-            <div class="comments">
-              <table id="bookComments${bookIndex}"></table>
-            </div>
-            <input id="commentInput${bookIndex}" type="text" placeholder="Bitte Kommentar eingeben...">
-            <img src="./assets/icons/115704_paperplane_paper plane_icon.png" alt="paper plane"  onclick="addComment(${bookIndex})">
-          </div>
-        </div>`;
-
-  if (books[bookIndex].liked == true) {
-    filledHeart(bookIndex);
-  } else {
-    unfilledHeart(bookIndex);
-  }
-  commentsTemplate(bookIndex);
-}
-
 function toggleHeart(bookIndex) {
   if (books[bookIndex].liked == true) {
     unfilledHeart(bookIndex);
@@ -88,24 +37,6 @@ function toggleHeart(bookIndex) {
     books[bookIndex].likes;
 }
 
-function commentsTemplate(bookIndex) {
-  let comments = document.getElementById("bookComments" + bookIndex);
-  comments.innerHTML = "";
-
-  for (
-    let commentsIndex = 0;
-    commentsIndex < books[bookIndex].comments.length;
-    commentsIndex++
-  ) {
-    comments.innerHTML += `
-    <tr>
-      <td>[${books[bookIndex].comments[commentsIndex].name}]</td>
-      <td>:</td>
-      <td>${books[bookIndex].comments[commentsIndex].comment}</td>
-    </tr>`;
-  }
-}
-
 function addComment(bookIndex) {
   let commentInputRef = document.getElementById("commentInput" + bookIndex);
   let commentInput = commentInputRef.value;
@@ -118,6 +49,8 @@ function addComment(bookIndex) {
     "name": "Gast",
     "comment": commentInput,
   });
+
   commentsTemplate(bookIndex);
+  
   commentInputRef.value = "";
 }
